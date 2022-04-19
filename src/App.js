f75bacc7dd17ea0ecd0a36353280e04414a245be
faha1999 <kafaha1999@gmail.com>
2022-04-20 00:24:40 +0600

    added figure & header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import Figure from './components/Figure';
+import { Header } from './components/Header';
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="game-container">
+        <Figure />
+      </div>
+    </div>
+  );
 }
 
 export default App;
