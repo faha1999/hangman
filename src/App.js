adcab4f2674bdb648b444edbca5991451e6a2bed
faha1999 <kafaha1999@gmail.com>
2022-04-20 00:59:23 +0600

    Implement State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Figure from './components/Figure';
 import { Header } from './components/Header';
 import { Word } from './components/Word';
 import { WrongLetter } from './components/WrongLetter';
+import { useState } from 'react';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
     <div className="App">
       <Header />
